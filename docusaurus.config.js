aa74bffa75b4cc3ef495dad4af08ee6d792707ef
bjwswang <bjwswang@gmail.com>
2023-05-12 14:26:57 +0800

    docs: add more items on footer
    Signed-off-by: bjwswang <bjwswang@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,59 +1,57 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'BESTCHAIN ',
-  tagline: '提供完全开源、全栈式的Web3解决方案',
-  favicon: 'img/favicon.ico',
+  title: "BESTCHAIN ",
+  tagline: "提供完全开源、全栈式的Web3解决方案",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://bestchains.github.io',
+  url: "https://bestchains.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/website/',
+  baseUrl: "/website/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'bestchains', // Usually your GitHub org/user name.
-  projectName: 'website', // Usually your repo name.
+  organizationName: "bestchains", // Usually your GitHub org/user name.
+  projectName: "website", // Usually your repo name.
 
-  deploymentBranch: 'gh-pages',
+  deploymentBranch: "gh-pages",
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'zh-Hans',
-    locales: ['zh-Hans', 'en'],
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans", "en"],
     localeConfigs: {
-      'zh-Hans': {
-        label: '中文',
+      "zh-Hans": {
+        label: "中文",
       },
       en: {
-        label: 'English',
-      }
+        label: "English",
+      },
     },
   },
 
-  plugins: [
-    'plugin-image-zoom'
-  ],
+  plugins: ["plugin-image-zoom"],
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
@@ -67,7 +65,7 @@ const config = {
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -77,45 +75,45 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       announcementBar: {
-        id: 'start',
+        id: "start",
         content:
-            '⭐️ If you like Bestchains, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/bestchains">GitHub</a>! ⭐️',
+          '⭐️ If you like Bestchains, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/bestchains">GitHub</a>! ⭐️',
       },
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: '',
+        title: "",
         logo: {
-          alt: 'Bestchains Logo',
-          src: 'img/bestchains-logo.png',
-          srcDark: 'img/bestchains-logo-dark.png',
+          alt: "Bestchains Logo",
+          src: "img/bestchains-logo.png",
+          srcDark: "img/bestchains-logo-dark.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'docsSidebar',
-            position: 'left',
-            label: '文档',
+            type: "docSidebar",
+            sidebarId: "docsSidebar",
+            position: "left",
+            label: "文档",
           },
           {
-            type: 'localeDropdown',
-            position: 'right',
+            type: "localeDropdown",
+            position: "right",
           },
           {
-            href: 'https://github.com/bestchains',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/bestchains",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       imageZoom: {
         // CSS selector to apply the plugin to, defaults to '.markdown img'
-        selector: '.markdown img',
+        selector: ".markdown img",
         // Optional medium-zoom options
         // see: https://www.npmjs.com/package/medium-zoom#options
         options: {
           margin: 24,
-          background: 'transparent',
+          background: "transparent",
           scrollOffset: 0,
           // Remove the options below, or it'll have 'Cannot read properties of null' error.
           // container: '#zoom-container',
@@ -123,48 +121,70 @@ const config = {
         },
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: '文档',
+            title: "Bestchain平台",
             items: [
               {
-                label: '产品简介',
-                to: '/docs/Introduction/overview',
+                label: "产品简介",
+                to: "/docs/Introduction/overview",
               },
               {
-                label: '快速开始',
-                to: '/docs/QuickStart/usage',
+                label: "快速开始",
+                to: "/docs/QuickStart/usage",
               },
               {
-                label: '用户指南',
-                to: '/docs/UserGuide/overview',
+                label: "用户指南",
+                to: "/docs/UserGuide/overview",
               },
               {
-                label: '开发指南',
-                to: '/docs/DevGuide/contract_dev',
+                label: "开发指南",
+                to: "/docs/DevGuide/contract_dev",
               },
             ],
           },
           {
-            title: '社区',
+            title: "SaaS",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/bestchains',
-              }
+                label: "数字存证服务",
+                to: "/docs/SaaS/dpsaas/intro",
+              },
             ],
           },
           {
-            title: '更多',
+            title: "工具",
+            items: [
+              {
+                label: "CLI",
+                to: "/docs/tools/cli",
+              },
+              {
+                label: "合约库",
+                to: "/docs/tools/contracts_lib",
+              },
+            ],
+          },
+          {
+            title: "社区",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/bestchains",
+              },
+            ],
+          },
+          {
+            title: "更多",
             items: [
               // {
               //   label: 'Blog',
               //   to: '/blog',
               // },
               {
-                label: 'GitHub',
-                href: 'https://github.com/bestchains',
+                label: "GitHub",
+                href: "https://github.com/bestchains",
               },
             ],
           },
